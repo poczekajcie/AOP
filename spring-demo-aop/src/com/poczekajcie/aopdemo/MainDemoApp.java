213dbae18521feae9b955f6e4c6eeaa7df6c4a1a
poczekajcie <krakowski.jacek97@wp.pl>
2019-02-22 08:46:03 +0100

    @before any class

--- spring-demo-aop/src/com/poczekajcie/aopdemo/MainDemoApp.java
+++ spring-demo-aop/src/com/poczekajcie/aopdemo/MainDemoApp.java
@@ -1,6 +1,7 @@
 package com.poczekajcie.aopdemo;
 
 import com.poczekajcie.aopdemo.dao.AccountDAO;
+import com.poczekajcie.aopdemo.dao.MembershipDAO;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MainDemoApp {
@@ -10,15 +11,13 @@ public class MainDemoApp {
         //read spring config java class
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
 
-        //get the bean from spring container
+        //get the beans from spring container
         AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+        MembershipDAO membershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
 
-        //call the business method
+        //call the methods
         accountDAO.addAccount();
-
-        System.out.println("Let's call it again");
-
-        accountDAO.addAccount();
+        membershipDAO.addAccount();
 
         //close the context
         context.close();
